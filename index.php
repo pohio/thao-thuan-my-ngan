e3f222c683f3dfb7be72194afe39157f3e5a7d76
ThaoKante <nhuthaodoan17@gmail.com>
2026-08-05 08:33:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <? echo"xin chao nay cua thuan"; ?>
-<<<<<<< HEAD
+ HEAD
 <? echo"xin chao nay cua my"; ?>
 <? echo"xin chao nay cua my 2"; ?>
 <? echo"xin chao nay cua my 3"; ?>
 <? echo"xin chao nay cua thao"; ?>
 
-<? echo"xin chao nay cua thao 1"; ?>
-=======
+<? echo"xin chao nay cua thao 16t66tttt7t"; ?>
+
 <? echo"xin chao nay cua thuan7777"; ?>
 
 
